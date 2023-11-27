ba26be5bdc44681a8b254b63b9a032fa51453b33
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2023-11-27 16:46:58 +0100

    add slurm setup

---
+++ Projects/DEL-MC2/slurm/run_nb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=8G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=performance
+#SBATCH --out=slurm/log_out.txt
+#SBATCH --err=slurm/log_err.txt
+#SBATCH --job-name="DELMC2"
+python3 -m pip install -r requirements.txt
+echo
+
+NB_PATH="fhnw-del-mc2.ipynb"
+python3 -m papermill $NB_PATH $NB_PATH -k 'python3'
+
+echo "Done!"
